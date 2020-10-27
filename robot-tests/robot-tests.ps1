d84a034e0e5a5721efeb0eae2a05f6107f0444f9
Florian Roth <venom14@gmail.com>
2020-10-27 20:10:39 +0100

    feat: new test

--- robot-tests/robot-tests.ps1
+++ robot-tests/robot-tests.ps1
@@ -9,12 +9,19 @@ $RaccineInstallerFolder = ".\Raccine"
 function Uninstall-Raccine {
     $Command = "$($RaccineInstallerFolder)\install-raccine.bat UNINSTALL"
     Invoke-Expression $Command
+    Start-Sleep -s 1
 }
 function Install-Raccine-LogOnly {
     $Command = "$($RaccineInstallerFolder)\install-raccine.bat FULL_SIMU"
     Invoke-Expression $Command
+    Start-Sleep -s 1
 }
 
-# Test 1
+# Test 1 - Block: Vssadmin Delete Shadows
 Uninstall-Raccine
 Install-Raccine-LogOnly
+Start-Sleep -s 1
+Invoke-Expression "vssadmin delete shadows"
+$Result = Get-EventLog -LogName Application -Message *Raccine* -Newest 1
+$Result.Message -Match "delete shadows"
+Start-Sleep -s 1
