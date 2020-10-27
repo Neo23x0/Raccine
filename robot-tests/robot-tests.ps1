2c0e6b67d6b95d1a461d189a9e10c3da9876e43f
Florian Roth <venom14@gmail.com>
2020-10-27 20:32:38 +0100

    feat: more and better checks

--- robot-tests/robot-tests.ps1
+++ robot-tests/robot-tests.ps1
@@ -16,12 +16,32 @@ function Install-Raccine-LogOnly {
     Invoke-Expression $Command
     Start-Sleep -s 1
 }
+function Is-Running($ProcessName) {
+    $process = Get-Process $ProcessName -ErrorAction SilentlyContinue
+    if ($process) {
+        return $False
+    }
+    return $True
+}
 
-# Test 1 - Block: Vssadmin Delete Shadows
+# Test 1 - Log Only : Vssadmin Delete Shadows
 Uninstall-Raccine
 Install-Raccine-LogOnly
 Start-Sleep -s 1
 Invoke-Expression "vssadmin delete shadows"
+# Check correct handling
+# Evenlog
 $Result = Get-EventLog -LogName Application -Message *Raccine* -Newest 1
-$Result.Message -Match "delete shadows"
+If ( $Result.Message -NotMatch "deleteX shadows" ) { 
+    Write-Host "Eventlog entry of detection not found"
+    exit 1 
+}
+# Killed process / not hanging process
+If ( Is-Running("vssadmin") ) { 
+    Write-Host "Process is still running"
+    exit 1
+}
 Start-Sleep -s 1
+
+# Uninstall Raccine
+Uninstall-Raccine
