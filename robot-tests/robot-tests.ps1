7796ecff3446a69221da55133642e8d3aa4b295b
Florian Roth <venom14@gmail.com>
2020-10-27 20:42:23 +0100

    fix: error in test script

--- robot-tests/robot-tests.ps1
+++ robot-tests/robot-tests.ps1
@@ -19,9 +19,9 @@ function Install-Raccine-LogOnly {
 function Is-Running($ProcessName) {
     $process = Get-Process $ProcessName -ErrorAction SilentlyContinue
     if ($process) {
-        return $False
+        return $True
     }
-    return $True
+    return $False
 }
 
 # Test 1 - Log Only : Vssadmin Delete Shadows
@@ -29,9 +29,11 @@ Uninstall-Raccine
 Install-Raccine-LogOnly
 Start-Sleep -s 1
 Invoke-Expression "vssadmin delete shadows"
+Start-Sleep -s 2
 # Check correct handling
 # Evenlog
 $Result = Get-EventLog -LogName Application -Message *Raccine* -Newest 1
+Write-Host $Result.Message
 If ( $Result.Message -NotMatch "delete shadows" ) { 
     Write-Host "Eventlog entry of detection not found"
     exit 1 
@@ -41,7 +43,8 @@ If ( Is-Running("vssadmin") ) {
     Write-Host "Process is still running"
     exit 1
 }
-Start-Sleep -s 1
+Start-Sleep -s 5
 
+# Cleanup
 # Uninstall Raccine
 Uninstall-Raccine
