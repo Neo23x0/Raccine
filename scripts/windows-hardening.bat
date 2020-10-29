c71478e43619f29ff2254640c6227430285064fa
Florian Roth <venom14@gmail.com>
2020-10-30 00:24:54 +0100

    fix: smartscreen block - manual rules update

--- scripts/windows-hardening.bat
+++ scripts/windows-hardening.bat
@@ -157,7 +157,7 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" /v SignSecu
 ::
 :: Enable SmartScreen
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v ShellSmartScreenLevel /t REG_SZ /d Block /f
+REM reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v ShellSmartScreenLevel /t REG_SZ /d Block /f
 ::
 :: Enforce device driver signing
 BCDEDIT /set nointegritychecks OFF
