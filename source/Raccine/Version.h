0c6e97e21d139603797b68d4217bcab119921ade
John Lambert <johnla@microsoft.com>
2020-11-13 13:27:19 -0800

    version info

---
+++ source/Raccine/Version.h
@@ -0,0 +1,21 @@
+#ifndef VERSIONINFO_H
+#define VERSIONINFO_H
+
+#define VER_FILEVERSION             1,4,0,0
+#define VER_FILEVERSION_STR         "1.4.0.0\0"
+
+#define VER_PRODUCTVERSION          1,4,0,0
+#define VER_PRODUCTVERSION_STR      "1.4.0.0\0"
+
+#define VER_COMPANYNAME_STR         "Raccine"
+#define VER_FILEDESCRIPTION_STR     "A Simple Ransomware Vaccine by Florian Roth. See https://github.com/Neo23x0/Raccine"
+#define VER_INTERNALNAME_STR        "Raccine"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright 2020"
+#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "Raccine.exe"
+#define VER_PRODUCTNAME_STR         "Raccine"
+
+#define VER_COMPANYDOMAIN_STR       "https://github.com/Neo23x0/Raccine"
+
+#endif 
