a375660ab6939e52bcaed370b389c36b0d09ea80
Florian Roth <venom14@gmail.com>
2020-11-13 20:23:04 +0100

    feat: .NET framework requirement

--- build_dist.bat
+++ build_dist.bat
@@ -13,6 +13,9 @@ powershell -executionpolicy bypass -Command "Expand-Archive -Path .\temp\yara_x8
 ECHO Downloading Visual C++ Runtime ...
 powershell -executionpolicy bypass -Command "(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/vs/16/release/vc_redist.x64.exe', '.\tools\vc_redist.x64.exe')"
 powershell -executionpolicy bypass -Command "(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x86.exe', '.\tools\vc_redist.x86.exe')" 
+:: Get .NET framework
+ECHO Downloading .NET Framework 
+powershell -executionpolicy bypass -Command "(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe', '.\tools\NDP462-KB3151800-x86-x64-AllOS-ENU.exe')"
 ECHO Create folder .\Raccine that will hold the installer package
 DEL /F Raccine.zip
 @RD /S /Q ".\Raccine"
@@ -46,11 +49,14 @@ XCOPY tools\yarac64.exe Raccine\yara\
 XCOPY tools\yara32.exe Raccine\yara\
 XCOPY tools\yarac32.exe Raccine\yara\
 
-
 :: Visual C++ Runtime
 ECHO Copying VC++ runtime to dist folder
 XCOPY tools\vc_redist.x64.exe Raccine\vc_redist.x64.exe*
 XCOPY tools\vc_redist.x86.exe Raccine\vc_redist.x86.exe*
+
+:: .NET Framework
+ECHO Copying .NET setup file ...
+XCOPY tools\NDP462-KB3151800-x86-x64-AllOS-ENU.exe Raccine\NDP462-KB3151800-x86-x64-AllOS-ENU.exe*
 
 :: GUI
 ECHO Copying GUI components to dist folder
